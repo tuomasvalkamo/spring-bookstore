2aa01d3d55cedfcb376a328fab402f8ce46fca2e
Tuomas Valkamo <valkamo.tuomas@gmail.com>
2021-02-04 05:44:05 +0200

    First commit

---
+++ src/main/java/hh/exercise/Bookstore/web/BookController.java
@@ -0,0 +1,13 @@
+package hh.exercise.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+	@GetMapping("/index")
+	public String bookstore(Model model) {
+		return "index";
+	}
+}
