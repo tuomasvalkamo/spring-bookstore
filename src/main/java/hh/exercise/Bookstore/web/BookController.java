06ed575fa99e1257a070afcd20917eb9670b56ac
Tuomas Valkamo <valkamo.tuomas@gmail.com>
2021-02-06 01:56:32 +0200

    Books can be edited

--- src/main/java/hh/exercise/Bookstore/web/BookController.java
+++ src/main/java/hh/exercise/Bookstore/web/BookController.java
@@ -16,25 +16,29 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 
-	@GetMapping("/booklist")
+	// Booklist page
+	@GetMapping({ "/", "/booklist" })
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 
 		return "booklist";
 	}
 
+	// Add book page
 	@GetMapping("/addbook")
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
 		return "addbook";
 	}
 
+	// Save new book
 	@PostMapping("/save")
 	public String save(Book book) {
 		repository.save(book);
 		return "redirect:booklist";
 	}
 
+	// Delete existing book
 	@GetMapping("/delete/{id}")
 	public String deleteBook(@PathVariable("id") long bookId, Model model) {
 		repository.deleteById(bookId);
@@ -42,10 +46,10 @@ public class BookController {
 		return "redirect:../booklist";
 	}
 
-	@GetMapping("/editbook/{id}")
-	public String editBook(@PathVariable("id") long bookId, Book existingBook, Model model) {
-		model.addAttribute("book", existingBook);
-		model.addAttribute("id", bookId);
+	// Edit existing book
+	@GetMapping("/edit/{id}")
+	public String editBook(@PathVariable("id") long bookId, Model model) {
+		model.addAttribute("book", repository.findById(bookId));
 		return "editbook";
 	}
 }
